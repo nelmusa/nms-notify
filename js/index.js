020be996bc99784bfeba42246a11acd25bebb9e8
Nelson Murillo <nel.mu.sa@gmail.com>
2017-02-17 16:28:42 -0600

    Cambios 2017-02-17

--- js/index.js
+++ js/index.js
@@ -16,19 +16,19 @@ var app = {
     },
     receivedEvent: function(id) {
         var idnotify = localStorage.idnotify;
-        if (idnotify == null || idnotify == "" || idnotify == undefined){
+        if (idnotify == null || idnotify == "null" || idnotify == "" || idnotify == undefined){
             window.FirebasePlugin.getToken(function(token) {
                 // save this server-side and use it to push notifications to this device
                 console.log('TOKEN FIREBASE : ' + token);
                 localStorage.idnotify = token;
                 d.style.display = "block";
-                dd.style.display = "block";
 
                 var vvv = document.getElementById('dd');
                 vvv.childNodes[0].nodeValue = token;
+                dd.style.display = "block";
 
                 var request = new XMLHttpRequest();
-                request.open("GET", "http://www.wai-news.com/index.php?option=com_jbackend&view=request&action=put&module=push&resource=register&token=" + token + "&appcode=nms.wai.001&platform=ios&ios_alert=1&ios_badge=1&ios_sound=1",
+                request.open("GET", "http://www.wai-news.com/index.php?option=com_jbackend&view=request&action=put&module=push&resource=register&token=" + token + "&appcode=com.nmsnotify.android&platform=ios&ios_alert=1&ios_badge=1&ios_sound=1",
                 true);
                 request.send();
                 f.style.display = "block";
